94fbd507cfa2f665594955b60568d4d6029529fc
Ravdeep Singh <ravdeep2004@gmail.com>
2025-10-31 16:21:54 +0530

    Added Updated changed code for finding duplicate elements in an array

--- Console-Project/arraydemo.js
+++ Console-Project/arraydemo.js
@@ -1,3 +1,10 @@
 const numbers = [1, 2, 2, 3, 4, 4, 7, 7, 11, 88, 9, 5, 9, 7, 12, 88];
 const uniqueNumbers = [...new Set(numbers)];
 console.log(uniqueNumbers);  
+
+// changes done to code
+// used diff method this time filter() and indexOf()
+
+const numbers1 = [1, 2, 2, 3, 4, 4, 7, 7, 11, 88, 9, 5, 9, 7, 12, 88];
+const uniqueNumbers1 = numbers1.filter((num, index) => numbers1.indexOf(num) === index);
+console.log(uniqueNumbers1);  // [1, 2, 3, 4, 5]
